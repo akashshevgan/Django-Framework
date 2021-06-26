83d13d0fc770588b07e5a29ec2485e6655740caa
akash <akash@MSI>
2021-06-26 10:31:28 +0530

    Refactor form field code

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    
+    return render(request, 'contact.html')
+
+    
+def index(request):
+    return render(request, 'index.html')
